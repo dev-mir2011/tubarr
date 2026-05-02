44c542627e4e4599e6bb9465adc6cdd06436df89
dev-mir2011 <ruhaanmir.w@gmail.com>
2026-05-02 18:36:55 +0530

    feat: added playlist support and mp3 file support

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory
-import threading
-import os
-import uuid
 import json
+import os
+import threading
+import uuid
 from urllib.parse import unquote
 
-from helper_functions import *
+from flask import Flask, jsonify, render_template, request, send_from_directory
+
+from helper_functions import (
+    DOWNLOAD_DIR,
+    YOUTUBE_DIR,
+    build_cache,
+    check_for_videos_in_channel,
+    clean_rss_feed,
+    generate_thumbnail_cache,
+    jobs,
+    run_download,
+    save_jobs,
+    scan_channels_once,
+    scan_playlists_once,
+    youtube_channel_url_to_rss_url,
+    youtube_playlist_url_to_rss_url,
+    youtube_video_url_to_id,
+)
 
 app = Flask(
     __name__, template_folder="templates", static_folder="static", static_url_path="/"
@@ -14,6 +30,8 @@ app = Flask(
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 os.makedirs(YOUTUBE_DIR, exist_ok=True)
 os.makedirs("data", exist_ok=True)
+
+# ! Frontend gets served here
 
 
 @app.route("/")
@@ -41,6 +59,9 @@ def settings():
     return render_template("settings.html")
 
 
+# ? API Routes Start Here
+
+
 @app.route("/api/download", methods=["POST"])
 def api_download():
     data = request.json
@@ -49,7 +70,9 @@ def api_download():
     if not url:
         return jsonify({"error": "url required"}), 400
 
-    jobs.append({"status": "queued", "url": url, "id": youtube_url_to_id(url)})
+    jobs.append(
+        {"status": "queued", "url": url, "id": youtube_channel_url_to_rss_url(url)}
+    )
     save_jobs()
 
     # optional parameters
@@ -76,17 +99,17 @@ def api_download():
             add_metadata,
             move_after,
             extra_args,
-            youtube_url_to_id(url),
+            youtube_video_url_to_id(url),
         ),
     )
 
     thread.start()
-    jobs[-1] = {"status": "started", "url": url, "id": youtube_url_to_id(url)}
+    jobs[-1] = {"status": "started", "url": url, "id": youtube_video_url_to_id(url)}
     save_jobs()
     return jsonify(
         {
             "job_id": jobs.index(
-                {"status": "started", "url": url, "id": youtube_url_to_id(url)}
+                {"status": "started", "url": url, "id": youtube_video_url_to_id(url)}
             ),
             "status": "started",
         }
@@ -109,7 +132,7 @@ def api_all_jobs():
     with open("data/jobs.json") as file:
         jobs = json.load(file)
 
-    if jobs == None:
+    if jobs is None:
         jobs = []
 
     return jsonify(jobs)
@@ -119,7 +142,7 @@ def api_all_jobs():
 def api_feed():
     data = request.get_json()
     url = data.get("url")
-    cleaned = clean_rss_feed(youtube_to_rss(url))
+    cleaned = clean_rss_feed(youtube_channel_url_to_rss_url(url))
 
     return jsonify(cleaned)
 
@@ -149,7 +172,7 @@ def api_subscribe():
         if not url:
             return jsonify({"error": "url required"}), 400
 
-        rss_url = youtube_to_rss(url)
+        rss_url = youtube_channel_url_to_rss_url(url)
 
         sub = {
             "id": str(uuid.uuid4()),
@@ -188,7 +211,7 @@ def api_subscribe():
 
 @app.route("/api/check_for_videos", methods=["GET"])
 def api_check_for_videos():
-    new_videos = check_for_videos()
+    new_videos = check_for_videos_in_channel()
     return jsonify({"code": 200, "New Videos": new_videos}), 200
 
 
@@ -249,11 +272,11 @@ def api_settings():
 @app.route("/api/videosDownloaded")
 def api_videos_downloaded():
     videos = []
+    VIDEO_EXTENSIONS = (".mkv", ".mp4", ".webm", ".mov", ".avi", ".mp3", ".m4a")
 
     for root, _, files in os.walk(YOUTUBE_DIR):
         for file in files:
-            if file.lower().endswith(".mkv"):
-
+            if file.lower().endswith(VIDEO_EXTENSIONS):
                 full_path = os.path.join(root, file)
 
                 rel_path = os.path.relpath(full_path, YOUTUBE_DIR)
@@ -278,13 +301,86 @@ def api_videos_downloaded():
 
 @app.route("/api/scanOnce")
 def api_scan_once():
-    scan_once()
+    scan_channels_once()
     return jsonify({"message": "Scaned", "code": 200}), 200
+
+
+@app.route("/api/subscribePlaylist", methods=["POST", "GET", "DELETE"])
+def api_subscribe_playlist():
+
+    global subscriptions
+
+    # safe load
+    try:
+        if os.path.exists("data/playlists.json"):
+            with open("data/playlists.json", "r") as f:
+                content = f.read().strip()
+                subscriptions = json.loads(content) if content else []
+        else:
+            subscriptions = []
+    except Exception:
+        subscriptions = []
+    if request.method == "GET":
+        return jsonify(subscriptions), 200
+
+    elif request.method == "POST":
+        data = request.get_json()
+        url = data.get("url")
+        prefrences = data.get("prefrences")
+        settings = data.get("settings")
+
+        if not url:
+            return jsonify({"error": "url required"}), 400
+
+        rss_url = youtube_playlist_url_to_rss_url(url)
+
+        sub = {
+            "id": str(uuid.uuid4()),
+            "youtube_playlist_url": url,
+            "rss_url": rss_url,
+            "last_seen_id": None,
+            "enabled": True,
+            "prefrences": prefrences,
+            "settings": settings,
+        }
+
+        subscriptions.append(sub)
+
+        with open("data/playlists.json", "w") as f:
+            json.dump(subscriptions, f, indent=2)
+
+        return jsonify(sub), 201
+
+    elif request.method == "DELETE":
+        data = request.get_json()
+        sub_id = data.get("id")
+
+        new_list = [s for s in subscriptions if s["id"] != sub_id]
+
+        if len(new_list) == len(subscriptions):
+            return jsonify({"error": "not found"}), 404
+
+        subscriptions = new_list
+
+        with open("data/playlists.json", "w") as f:
+            json.dump(subscriptions, f, indent=2)
+
+        return jsonify({"message": "removed"}), 200
+
+    return jsonify({"message": "405 Method Not Allowed"}), 405
+
+
+@app.route("/api/scanPlaylistsOnce")
+def api_scan_playlists_once():
+    scan_playlists_once()
+    return jsonify({"message": "Scan Complete", "code": 200}), 200
 
 
 if __name__ == "__main__":
     debug = os.getenv("DEBUG", "false").lower() == "true"
-    threading.Thread(target=check_for_videos, daemon=True, name="Video Scanner").start()
+    threading.Thread(
+        target=check_for_videos_in_channel, daemon=True, name="Video Scanner"
+    ).start()
     threading.Thread(
         target=generate_thumbnail_cache, daemon=True, name="Thumbnail Generator"
     ).start()
--- helper_functions.py
+++ helper_functions.py
@@ -1,15 +1,16 @@
+import json
 import os
 import re
-import json
 import shutil
 import subprocess
-import threading
 import sys
-from urllib.parse import urlparse, parse_qs
-import feedparser
-import requests
+import threading
 import time
 from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+
+import feedparser
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,6 +31,20 @@ and feature functions are :-
 ? youtube_url_to_id()
 ? youtube_to_rss()
 ? check_for_videos()
+"""
+
+"""
+! Metadata handeling functions :-
+save_jobs()
+move_to_media()
+clean_rss_feed()
+youtube_video_url_to_id()
+youtube_channel_url_to_rss_url()
+youtube_playlist_url_to_rss_url()
+check_for_videos()
+scan_once()
+build_cache()
+generate_thumbnail_cache()
 """
 
 jobs = []
@@ -141,7 +156,7 @@ def clean_rss_feed(link: str):
     return videos
 
 
-def youtube_url_to_id(url):
+def youtube_video_url_to_id(url):
     parsed = urlparse(url)
     video_id = parse_qs(parsed.query).get("v", [None])[0]
     if not video_id:
@@ -149,7 +164,7 @@ def youtube_url_to_id(url):
     return f"yt:video:{video_id}"
 
 
-def youtube_to_rss(url: str) -> str | None:
+def youtube_channel_url_to_rss_url(url: str) -> str | None:
     headers = {"User-Agent": "Mozilla/5.0"}
     html = requests.get(url, headers=headers, timeout=10).text
 
@@ -165,7 +180,27 @@ def youtube_to_rss(url: str) -> str | None:
     return f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
 
 
-def check_for_videos():
+def youtube_playlist_url_to_rss_url(url: str) -> str | None:
+    parsed = urlparse(url)
+
+    if "youtube.com" in parsed.netloc:
+        query = parse_qs(parsed.query)
+
+        if "list" in query:
+            playlist_id = query["list"][0]
+            return f"https://www.youtube.com/feeds/videos.xml?playlist_id={playlist_id}"
+
+    if "youtu.be" in parsed.netloc:
+        query = parse_qs(parsed.query)
+
+        if "list" in query:
+            playlist_id = query["list"][0]
+            return f"https://www.youtube.com/feeds/videos.xml?playlist_id={playlist_id}"
+
+    return None
+
+
+def check_for_videos_in_channel():
     global jobs
 
     with open("data/settings.json") as f:
@@ -220,70 +255,90 @@ def check_for_videos():
         time.sleep(int(settings["channel_scan_interval"]))
 
 
-def scan_once():
+def scan_channels_once():
     global jobs
 
     with open("data/channels.json") as f1:
-            channels = json.load(f1)
+        channels = json.load(f1)
 
     with open("data/jobs.json") as f2:
-            jobs = json.load(f2)
+        jobs = json.load(f2)
 
     existing_ids = {job["id"] for job in jobs}
 
     for channel in channels:
         videos = clean_rss_feed(channel["rss_url"])
         if not videos:
-                continue
+            continue
 
         latest = videos[0]
 
         if latest["id"] not in existing_ids:
             job = {
-                    "status": "started",
-                    "url": latest["url"],
-                    "id": latest["id"],
-                }
+                "status": "started",
+                "url": latest["url"],
+                "id": latest["id"],
+            }
 
             jobs.append(job)
             save_jobs()
 
             thread = threading.Thread(
-                        target=run_download,
-                        name=latest["id"],
-                        args=(
-                            latest["url"],
-                            channel["prefrences"]["output_dir"],
-                            channel["prefrences"]["audio_only"],
-                            channel["prefrences"]["audio_format"],
-                            channel["prefrences"]["filename_template"],
-                            channel["prefrences"]["embed_metadata"],
-                            channel["prefrences"]["embed_thumbnail"],
-                            channel["prefrences"]["add_metadata"],
-                            channel["prefrences"]["move_after"],
-                            channel["prefrences"]["extra_args"],
-                            latest["id"],
-                        ),
-                    )
+                target=run_download,
+                name=latest["id"],
+                args=(
+                    latest["url"],
+                    channel["prefrences"]["output_dir"],
+                    channel["prefrences"]["audio_only"],
+                    channel["prefrences"]["audio_format"],
+                    channel["prefrences"]["filename_template"],
+                    channel["prefrences"]["embed_metadata"],
+                    channel["prefrences"]["embed_thumbnail"],
+                    channel["prefrences"]["add_metadata"],
+                    channel["prefrences"]["move_after"],
+                    channel["prefrences"]["extra_args"],
+                    latest["id"],
+                ),
+            )
 
             thread.start()
 
+
 THUMB_DIR = f"{CACHE_DIR}/thumb"
 
+VIDEO_EXTENSIONS = (".mkv", ".mp4", ".webm", ".mov", ".avi")
+AUDIO_EXTENSIONS = (".mp3", ".m4a", ".flac", ".wav", ".ogg")
 
-VIDEO_EXTENSIONS = (".mkv", ".mp4", ".webm", ".mov", ".avi", ".mp3", ".m4a")
+
+def make_placeholder(thumb_path: str):
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-f",
+            "lavfi",
+            "-i",
+            "color=c=gray:s=320x180",
+            "-frames:v",
+            "1",
+            "-y",
+            thumb_path,
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
 
 def build_cache():
-    for root, dirs, files in os.walk(YOUTUBE_DIR):
+    for root, _, files in os.walk(YOUTUBE_DIR):
         for file in files:
-            if not file.lower().endswith(VIDEO_EXTENSIONS):
+            ext = os.path.splitext(file)[1].lower()
+
+            if ext not in VIDEO_EXTENSIONS + AUDIO_EXTENSIONS:
                 continue
 
-            video_path = os.path.join(root, file)
+            media_path = os.path.join(root, file)
 
-            # preserve folder structure
-            rel_path = os.path.relpath(video_path, YOUTUBE_DIR)
+            rel_path = os.path.relpath(media_path, YOUTUBE_DIR)
             thumb_rel = os.path.splitext(rel_path)[0] + ".jpg"
             thumb_path = os.path.join(THUMB_DIR, thumb_rel)
 
@@ -294,23 +349,49 @@ def build_cache():
 
             print("Generating:", thumb_rel)
 
-            subprocess.run(
-                [
-                    "ffmpeg",
-                    "-ss",
-                    "3",
-                    "-i",
-                    video_path,
-                    "-frames:v",
-                    "1",
-                    "-vf",
-                    "scale=320:-1",
-                    "-y",
-                    thumb_path,
-                ],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
+            if ext in VIDEO_EXTENSIONS:
+                result = subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-ss",
+                        "3",
+                        "-i",
+                        media_path,
+                        "-frames:v",
+                        "1",
+                        "-vf",
+                        "scale=320:-1",
+                        "-y",
+                        thumb_path,
+                    ],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.PIPE,
+                )
+
+                if result.returncode != 0:
+                    print("Video thumb failed:", file)
+                    make_placeholder(thumb_path)
+
+            else:
+                result = subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-i",
+                        media_path,
+                        "-an",
+                        "-vcodec",
+                        "copy",
+                        "-map",
+                        "0:v:0",
+                        "-y",
+                        thumb_path,
+                    ],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.PIPE,
+                )
+
+                if result.returncode != 0:
+                    make_placeholder(thumb_path)
 
 
 def generate_thumbnail_cache():
@@ -321,5 +402,91 @@ def generate_thumbnail_cache():
         time.sleep(int(settings["generate_thumbnail_cache_interval"]))
 
 
+# ? Playlist scanning and monitoring and downloading
+
+
+def get_playlist_data(url: str):
+    result = subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "yt_dlp",
+            "--flat-playlist",
+            "--dump-single-json",
+            url,
+        ],
+        capture_output=True,
+        text=True,
+    )
+
+    if result.returncode != 0:
+        raise RuntimeError(result.stderr)
+
+    data = json.loads(result.stdout)
+
+    final_data = data["entries"]
+
+    return final_data
+
+
+def playlist_data_cleanup(entries: list):
+    new_data = []
+    for entry in entries:
+        new_data.append({"id": f"yt:video:{entry['id']}", "url": entry["url"]})
+
+    return new_data
+
+
+def scan_playlists_once():
+    global jobs
+
+    with open("data/playlists.json") as f1:
+        playlists = json.load(f1)
+
+    with open("data/jobs.json") as f2:
+        jobs = json.load(f2)
+
+    existing_ids = {job["id"] for job in jobs}
+
+    for playlist in playlists:
+        videos = playlist_data_cleanup(
+            get_playlist_data(playlist["youtube_playlist_url"])
+        )
+        if not videos:
+            continue
+
+        latest = videos[-1]
+
+        if latest["id"] not in existing_ids:
+            job = {
+                "status": "started",
+                "url": latest["url"],
+                "id": latest["id"],
+            }
+
+            jobs.append(job)
+            save_jobs()
+
+            thread = threading.Thread(
+                target=run_download,
+                name=latest["id"],
+                args=(
+                    latest["url"],
+                    playlist["prefrences"]["output_dir"],
+                    playlist["prefrences"]["audio_only"],
+                    playlist["prefrences"]["audio_format"],
+                    playlist["prefrences"]["filename_template"],
+                    playlist["prefrences"]["embed_metadata"],
+                    playlist["prefrences"]["embed_thumbnail"],
+                    playlist["prefrences"]["add_metadata"],
+                    playlist["prefrences"]["move_after"],
+                    playlist["prefrences"]["extra_args"],
+                    latest["id"],
+                ),
+            )
+
+            thread.start()
+
+
 if __name__ == "__main__":
-    build_cache()
+    print(scan_playlists_once())
